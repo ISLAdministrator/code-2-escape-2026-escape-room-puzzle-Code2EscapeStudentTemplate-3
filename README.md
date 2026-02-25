217833d47cad7ff75ddee7dcd8c1d70dc7332d22
Lee Shawver <165132276+LeeShawverISL@users.noreply.github.com>
2026-02-25 14:47:17 +0000

    added background and buttons

--- README.md
+++ README.md
@@ -6,10 +6,13 @@ Files:
 - `index.html` – page content
 - `style.css` – visual style
 - `script.js` – interactivity
+- `background1.jpg` – example background image
+- `start_button.png` – example button image
+
 
 Rules:
 - Use vanilla JavaScript only
-- Do not rename files
+- Do not rename the .html, .js, or .css files
 - Commit your changes regularly
 
 Your teacher will publish the project later.
--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 console.log("Script loaded");
 
-// below is basic example code for using a button
-// you can use it, expand on it, or delete it
-// that all depends on your team's plan.
-
 const scene = document.getElementById("scene");
 const imageButton = document.getElementById("imageButton");
+const textBox = document.getElementById("textBox");
 
 imageButton.addEventListener("click", function () {
   scene.style.backgroundImage = 'url("background2.jpg")';
   imageButton.style.display = "none";
-});
+  textBox.style.display = "none";
 });
